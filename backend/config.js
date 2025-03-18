d43bef8b299bef6db4a433f1bc18b551668e536b
VishvadiniRavihari <120405053+Zury7@users.noreply.github.com>
2025-03-18 09:19:31 +0530

    backend initialized | db connected

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 8000;
+
+export const mongoDBURL = 'mongodb+srv://root:root@bookstore-mern.3fhry.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN';
